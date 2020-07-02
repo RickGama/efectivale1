66f718208a846d7fbc8b071475d3f2b3fdae04a6
RickGama <66316588+RickGama@users.noreply.github.com>
2020-07-02 18:00:57 -0500

    Add files via upload

---
+++ SISOP/src/main/java/mx/com/efectivale/SisopApplication.java
@@ -0,0 +1,13 @@
+package mx.com.efectivale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisopApplication.class, args);
+	}
+
+}
---
+++ SISOP/src/test/java/mx/com/efectivale/SisopApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.efectivale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
